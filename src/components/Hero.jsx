783ef5d7ad9768d4f22d8cf6ff3f70616977ecbd
farhan sherin <farhana23sherin9023@gmail.com>
2026-04-03 21:20:47 +0530

    reduced the time delay in hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -59,7 +59,7 @@ export const Hero = () => {
                         initial={{ opacity: 0 }}
                         animate={{ opacity: 1 }}
                         exit={{ opacity: 0 }}
-                        transition={{ duration: 0.5, ease: "easeInOut" }}
+                        transition={{ duration: 0.2, ease: "easeInOut" }}
                         className="absolute inset-0 w-full h-full"
                     >
                         {/* Background Image with Continuous Cinematic Zoom */}
